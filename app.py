c9d1fd3fa40a3decf1e636d4e37382465c04b0ef
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2025-02-24 18:27:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+# Cambia esta línea
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-
-
 from dotenv import load_dotenv
 
 load_dotenv()
